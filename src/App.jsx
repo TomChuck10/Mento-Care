339e9df2a58174a3610c38978d19dff8a6943974
TomChuck <m.tomczyk@gymmanager.io>
2025-05-30 18:20:47 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
             style={{
               minHeight: "100dvh",
               // paddingTop: "max(10px, env(safe-area-inset-top))",
-              paddingBottom: "max(40px, env(safe-area-inset-bottom))",
+              paddingBottom: "max(80px, env(safe-area-inset-bottom))",
             }}
           >
             <div className="flex flex-col items-center justify-end h-full mt-auto z-30">
@@ -284,7 +284,7 @@ function App() {
             style={{
               minHeight: "100dvh",
               // paddingTop: "max(10px, env(safe-area-inset-top))",
-              paddingBottom: "max(40px, env(safe-area-inset-bottom))",
+              paddingBottom: "max(80px, env(safe-area-inset-bottom))",
             }}
           >
             {/* Background Image */}
@@ -575,7 +575,7 @@ function App() {
             style={{
               minHeight: "100dvh",
               // paddingTop: "max(10px, env(safe-area-inset-top))",
-              paddingBottom: "max(40px, env(safe-area-inset-bottom))",
+              paddingBottom: "max(80px, env(safe-area-inset-bottom))",
             }}
           >
             <div className="h-full overflow-y-auto flex flex-col pt-10 pb-20">
